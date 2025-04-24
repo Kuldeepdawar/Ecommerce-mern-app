472cd37d9a5b90bc08e1cbf291a9095ca6ae2dfd
KuldeepDawar <kuldeepdawar47@gmail.com>
2025-04-25 08:57:30 +1000

    created product controller file

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+// Function to list all products
+const listProducts = async (req, res) => {
+  // Implementation goes here
+};
+
+// Function to add a product
+const addProduct = async (req, res) => {
+  // Implementation goes here
+};
+
+// Function to remove a product
+const removeProduct = async (req, res) => {
+  // Implementation goes here
+};
+
+// Function to get a single product's information
+const singleProduct = async (req, res) => {
+  // Implementation goes here
+};
+
+// Exporting all product-related functions
+export { listProducts, addProduct, removeProduct, singleProduct };
